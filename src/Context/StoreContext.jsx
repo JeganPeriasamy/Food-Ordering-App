e723ee510ad811d6ae5426ee21f27834530a1b16
JEGAN PERIASAMY <166896131+JeganPeriasamy@users.noreply.github.com>
2024-05-20 13:09:31 +0530

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider = (props) => {
 
 
             const[cartItems,setCartItems] = useState({})
-            const url = "http://localhost:4000"
+            const url = "https://food-ordering-app-backend-1-hnpj.onrender.com/"
             const [token,setToken]  = useState("")
 
 
